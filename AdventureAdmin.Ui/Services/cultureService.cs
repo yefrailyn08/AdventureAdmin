0a5a57d42366befcadb7e7358e98ea778f76b68c
Yefrailyn08 <yefrailyn08@gmail.com>
2026-04-01 23:40:36 -0300

    Agrega servicios

--- AdventureAdmin.Ui/Services/cultureService.cs
+++ AdventureAdmin.Ui/Services/cultureService.cs
@@ -1,20 +1,28 @@
-﻿using AdventureAdmin.Data.Context;
+using AdventureAdmin.Data.Context;
 using Aplicada1.Core;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace AdventureAdmin.Ui.Services;
 
+
 public class CultureService(
-   AdventureWorksContext context
-    ) : IService<Data.Models.Culture, int>
+    AdventureWorksContext context
+    ) : IService<Data.Models.Culture, string>
 {
-    public Task<Data.Models.Culture?> Buscar(int id)
+    public async Task<bool> Guardar(Data.Models.Culture nuevaCulture)
+    {
+        await context.Cultures.AddAsync(nuevaCulture);
+        var cantidad = await context.SaveChangesAsync();
+        return cantidad > 0;
+    }
+
+    public Task<Data.Models.Culture?> Buscar(string id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<bool> Eliminar(int id)
+    public Task<bool> Eliminar(string id)
     {
         throw new NotImplementedException();
     }
@@ -22,14 +30,7 @@ public class CultureService(
     public async Task<List<Data.Models.Culture>> GetList(Expression<Func<Data.Models.Culture, bool>> criterio)
     {
         return await context.Cultures
-             .Where(criterio)
-             .ToListAsync();
-    }
-
-    public async Task<bool> Guardar(Data.Models.Culture entidad)
-    {
-        await context.Cultures.AddAsync(entidad);
-        var cantidad = await context.SaveChangesAsync();
-        return cantidad > 0;
+            .Where(criterio)
+            .ToListAsync();
     }
 }
